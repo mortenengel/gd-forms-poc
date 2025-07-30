de71e05f9977a97636851484a8aed99a291f024e
Morten Engel <me@alpha-solutions.dk>
2025-07-30 13:09:57 +0200

    added api token

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,12 @@ var FormsRepository = require('../repo/forms');
 const router = express.Router();
 
 router.post('/persist', async function(req, res, next) {
+  const apiToken = req.header("api-token");
+  if (process.env.API_TOKEN !== apiToken) {
+      next(new Error("Unauthorized."));
+      return;
+  }
+  
   try {
     const submission = req.body
     await FormsRepository.persist(submission)
